0bbbd9d54b01aa091673ed01ad066c497e9d07cc
JeffG9907 <jcagua4477@utm.edu.ec>
2025-05-30 15:56:48 -0500

    Cambios en rendimiento

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { Helmet } from 'react-helmet';
 import bannerImage from '../assets/banner.webp';
 import Typography from '@mui/material/Typography';
 import Navbar from '../components/Navbar';
-
 
 const MainLayout = ({ children }) => {
   const location = useLocation();
@@ -14,23 +14,29 @@ const MainLayout = ({ children }) => {
     <Box>
       <Navbar />
       {isHomePage && (
-        <Box className="banner-container">
-          <img 
-            src={bannerImage}
-            alt="Banner de la tienda"
-            loading="lazy"
-            className="banner-image"
-            width="1920" height="835"
-          />
-          <Typography variant="h2" className="slogan">
-            <span className="slogan-main" style={{display: "inline"}}>
-              IMPON<span className="slogan-orange">E</span>NTE, 
-            </span>
-            <span className="slogan-sub" style={{display: "inline", marginLeft: "0.5em"}}>
-              como t<span className="slogan-orange">ú</span>
-            </span>
-          </Typography>
-        </Box>
+        <>
+          {/* Preload del banner */}
+          <Helmet>
+            <link rel="preload" as="image" href={bannerImage} />
+          </Helmet>
+          <Box className="banner-container">
+            <img 
+              src={bannerImage}
+              alt="Banner de la tienda"
+              className="banner-image"
+              width="1920" height="835"
+              // loading="lazy" eliminado para optimizar LCP
+            />
+            <Typography variant="h2" className="slogan">
+              <span className="slogan-main" style={{display: "inline"}}>
+                IMPON<span className="slogan-orange">E</span>NTE, 
+              </span>
+              <span className="slogan-sub" style={{display: "inline", marginLeft: "0.5em"}}>
+                como t<span className="slogan-orange">ú</span>
+              </span>
+            </Typography>
+          </Box>
+        </>
       )}
       {children}
     </Box>

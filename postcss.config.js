432a3fe79bc8d31eae53572b08022ac8a8ad0cd1
JeffG9907 <jcagua4477@utm.edu.ec>
2025-06-05 16:10:38 -0500

    Cambios en el servidor de la página

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.js', './public/index.html'],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-  ]
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          require('@fullhuman/postcss-purgecss')({
+            content: [
+              './src/**/*.js',
+              './src/**/*.jsx',
+              './src/**/*.ts',
+              './src/**/*.tsx',
+              './public/index.html',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }),
+        ]
+      : []),
+  ],
 };
